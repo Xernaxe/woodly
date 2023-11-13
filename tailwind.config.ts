fc64fe1077362da994f6d430c06bdccea4c40f41
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2023-11-13 20:05:43 +0200

    skeleton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-dBlue": "linear-gradient(118deg, rgba(10,31,53,1) 0%, rgba(4,13,23,1) 100%)",
+				"gradient-wBlue": "linear-gradient(118deg, #346296 0%, #081729 100%)",
+				"gradient-buttonL": "linear-gradient(97deg, #144558 -3.49%, #0E3B4C 118.41%)"
+			},
+			colors: {
+				"accentGreen": "#04B939",
+				"accentBlue": "#1C89EF",
+				"accentGrey": "#ffffff33",
+			},
+			screens: {
+				"mobile": "375px",
+				"tablet": "768px",
+				"desktop": "1024px",
+				"desktopL": "1440px"
+			},
+			fontFamily: {
+				"inter": ["Inter", ...defaultTheme.fontFamily.sans],
+				"outfit": ["Outfit", "Inter", ...defaultTheme.fontFamily.sans]
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;

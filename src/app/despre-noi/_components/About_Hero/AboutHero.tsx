773f147d6d24d01fb294f46ca3d51e2a08e1ac1b
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2023-11-23 02:22:50 +0200

    Update AboutHero.tsx

--- src/app/despre-noi/_components/About_Hero/AboutHero.tsx
+++ src/app/despre-noi/_components/About_Hero/AboutHero.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 export const AboutHero = () => {
 	return (
-		<section className='sectionP pt-20 relative bg-black desktop:pt-40 '>
-			<div className='m-auto desktop:w-10/12'>
+		<section className='flex flex-col pt-20 relative bg-black desktop:pt-40 desktop:pb-0 '>
+			<div className='paddingsX m-auto desktop:w-10/12'>
 				<h3 className='uppercase text-xs text-center font-light [letter-spacing:0.144rem] text-white'>
 					Despre Noi
 				</h3>
@@ -21,13 +21,15 @@ export const AboutHero = () => {
 				</p>
 			</div>
 
-			<div className='pt-4'>
-				<div className='relative h-[90vw] desktop:h-[30vw]'>
-					<div className='relative  w-full aspect-square'>
-						<Image src={'/about_1.png'} alt={'@TODO: Change'} fill />
-					</div>
-					<div className='hidden  w-full aspect-square desktop:block'>
-						<Image src={'/about_2.png'} alt={'@TODO: Change'} fill />
+			<div className=' py-4 desktop:pb-0'>
+				<div className='desktop:bg-[linear-gradient(0deg,_rgba(255,255,255,1)_0%,_rgba(255,255,255,1)_50%,_rgba(0,0,0,1)_50%,_rgba(0,0,0,1)_100%)] desktop:translate-y-1'>
+					<div className='paddingsX relative flex gap-8 h-[90vw] desktop:h-[30vw]'>
+						<div className='relative  w-full aspect-square'>
+							<Image src={'/about_1.png'} alt={'@TODO: Change'} fill />
+						</div>
+						<div className='hidden relative  w-full aspect-square desktop:block'>
+							<Image src={'/about_2.png'} alt={'@TODO: Change'} fill />
+						</div>
 					</div>
 				</div>
 			</div>

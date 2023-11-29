c3071f2a5de8fa38a2e2d2f566022de2d2f1dc84
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2023-11-29 16:08:23 +0200

    hero responsiveness

--- src/app/_components/Hero/Hero.tsx
+++ src/app/_components/Hero/Hero.tsx
@@ -5,16 +5,16 @@ import { MainButton } from '../_globals/MainButton/MainButton';
 export const Hero = () => {
 	return (
 		<section className='sectionP max-desktop:pt-20 relative desktop:pt-32 desktop:h-screen desktop:justify-center'>
-			<div className='relative tablet:ml-10 tablet:w-[65%] tablet:before:w-[1px] tablet:before:h-full tablet:before:bg-accentBlue tablet:before:flex tablet:before:absolute tablet:before:left-0 tablet:before:-translate-x-10 desktop:before:-left-10 desktop:ml-20 desktop:w-1/2'>
+			<div className='relative tablet:ml-10 tablet:w-[65%] tablet:before:w-[1px] tablet:before:h-full tablet:before:bg-accentBlue tablet:before:flex tablet:before:absolute tablet:before:left-0 tablet:before:-translate-x-10 desktop:before:-left-10 desktop:ml-20  desktop:w-1/2'>
 				<div className=' flex items-center gap-2'>
-					<p className='text-white font-light uppercase text-xs min-w-max tablet:letter-spacing'>
+					<p className='text-white font-light uppercase text-xs min-w-max tablet:letter-spacing desktop:text-sm'>
 						Firma de amenajari interioare
 					</p>
 					<span className='block h-[1px] w-full bg-accentGreen'></span>
 				</div>
 
 				<div className='flex flex-col gap-3 my-4 tablet:my-10 tablet:gap-4 desktop:gap-6'>
-					<h1 className='text-white font-light text-2xl leading-snug desktopL:text-5xl mobile:text-[clamp(1.5rem,4.5vw,3rem)] desktop:text-5xl'>
+					<h1 className='text-white font-light text-2xl leading-snug mobile:text-[clamp(1.5rem,4.5vw,3rem)] desktop:text-4xl desktopL:text-[clamp(2.5rem,3.3vw,3rem)] desktop:leading-[1.3]'>
 						Amenajam locuri in care fiecare detaliu te inspira.
 					</h1>
 					<p className='text-white font-light text-xs leading-relaxed mobile:text-[clamp(.75rem,2.5vw,1rem)] '>

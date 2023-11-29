01197e320ba47a6b6fd8c9992b13dac484edf4bb
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2023-11-30 00:22:10 +0200

    Update Header.tsx

--- src/app/_components/_globals/Header/Header.tsx
+++ src/app/_components/_globals/Header/Header.tsx
@@ -14,11 +14,13 @@ export const Header = () => {
 	];
 
 	return (
-		<header className='absolute z-10 top-0 left-0 w-screen py-2 px-6 tablet:px-[8%] desktop:border-b border-accentGrey border-solid desktop:py-[1.88rem] desktopL:px-[12%]'>
-			<NavbarDesktop navLinks={navLinks} />
-			<BurgerMenuProvider>
-				<NavbarMobile navLinks={navLinks} />
-			</BurgerMenuProvider>
+		<header className='absolute z-10 top-0 left-0 w-screen py-2 px-6 tablet:px-[8%] desktop:border-b border-accentGrey border-solid desktop:py-[1.88rem] desktopL:px-[12%] '>
+			<div className='max-w-[1093px] m-auto'>
+				<NavbarDesktop navLinks={navLinks} />
+				<BurgerMenuProvider>
+					<NavbarMobile navLinks={navLinks} />
+				</BurgerMenuProvider>
+			</div>
 		</header>
 	);
 };

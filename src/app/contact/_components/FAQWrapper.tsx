6955ff8b65cb0462b1145746a5b29c810b31a82b
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2023-12-07 19:15:50 +0200

    JSON-LD structured data FAQ

--- src/app/contact/_components/FAQWrapper.tsx
+++ src/app/contact/_components/FAQWrapper.tsx
@@ -3,6 +3,53 @@ import { IFAQ } from '@/app/_types/IFAQ';
 import { FAQCard } from './FAQCard';
 
 export const FAQWrapper = () => {
+	const structuredDataToAdd = {
+		'@context': 'https://schema.org',
+		'@type': 'FAQPage',
+		mainEntity: [
+			{
+				'@type': 'Question',
+				name: 'Cum pot obține o estimare a costurilor pentru proiectul meu?',
+				acceptedAnswer: {
+					'@type': 'Answer',
+					text: '<p>Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.</p>',
+				},
+			},
+			{
+				'@type': 'Question',
+				name: 'Ce set de abilități și experiență au membrii echipei dumneavoastră?',
+				acceptedAnswer: {
+					'@type': 'Answer',
+					text: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
+				},
+			},
+			{
+				'@type': 'Question',
+				name: 'Ce stiluri și tendințe în design interior preferați să explorați?',
+				acceptedAnswer: {
+					'@type': 'Answer',
+					text: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
+				},
+			},
+			{
+				'@type': 'Question',
+				name: 'Cum pot programa o consultație inițială?',
+				acceptedAnswer: {
+					'@type': 'Answer',
+					text: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
+				},
+			},
+			{
+				'@type': 'Question',
+				name: 'Cât durează în general un proiect de design interior?',
+				acceptedAnswer: {
+					'@type': 'Answer',
+					text: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
+				},
+			},
+		],
+	};
+
 	const FAQCards: IFAQ[] = [
 		{
 			title: 'Cum pot obține o estimare a costurilor pentru proiectul meu?',
@@ -32,16 +79,33 @@ export const FAQWrapper = () => {
 		<section className='sectionM gap-4'>
 			<div className='desktop:m-auto desktop:w-[80%]'>
 				<div className='flex flex-col gap-6 max-w-[26rem] m-auto mb-12'>
-					<h2 className='text-4xl font-light text-center'>Întrebări Frecvente</h2>
+					<h2 className='text-4xl font-light text-center'>
+						Întrebări Frecvente
+					</h2>
 					<p className='text-textGrey  text-center'>
 						Aveți întrebări? Noi avem răspunsuri. Dacă nu găsiți ceea ce
-						căutați, nu ezitați să <span className='text-accentGreen underline cursor-pointer'>ne contactați</span>.
+						căutați, nu ezitați să{' '}
+						<span className='text-accentGreen underline cursor-pointer'>
+							ne contactați
+						</span>
+						.
 					</p>
 				</div>
 				{FAQCards.map((card, index) => (
-					<FAQCard key={card.title} card={card} index={index} arrLength={FAQCards.length} />
+					<FAQCard
+						key={card.title}
+						card={card}
+						index={index}
+						arrLength={FAQCards.length}
+					/>
 				))}
 			</div>
+			<script
+				type='application/ld+json'
+				dangerouslySetInnerHTML={{
+					__html: JSON.stringify(structuredDataToAdd),
+				}}
+			/>
 		</section>
 	);
 };

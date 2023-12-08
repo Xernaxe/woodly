38184a7376294912fad64e04dfe2e3ca8944533f
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2023-12-08 03:05:36 +0200

    Dynamic structure data FAQ

--- src/app/(main)/contact/_components/FAQWrapper.tsx
+++ src/app/(main)/contact/_components/FAQWrapper.tsx
@@ -5,85 +5,26 @@ import Link from 'next/link';
 import { getAllFAQs } from '../../../../../sanity/sanity-utils';
 
 export default async function FAQWrapper() {
-	const structuredDataToAdd = {
-		'@context': 'https://schema.org',
-		'@type': 'FAQPage',
-		mainEntity: [
-			{
-				'@type': 'Question',
-				name: 'Cum pot obține o estimare a costurilor pentru proiectul meu?',
-				acceptedAnswer: {
-					'@type': 'Answer',
-					text: '<p>Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.</p>',
-				},
+	
+	const constructStructureData = (FAQCards: IFAQ[]) => {
+		const mainEntity = FAQCards.map((card) => ({
+			'@type': 'Question',
+			name: card.question,
+			acceptedAnswer: {
+				'@type': 'Answer',
+				text: card.answer,
 			},
-			{
-				'@type': 'Question',
-				name: 'Ce set de abilități și experiență au membrii echipei dumneavoastră?',
-				acceptedAnswer: {
-					'@type': 'Answer',
-					text: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-				},
-			},
-			{
-				'@type': 'Question',
-				name: 'Ce stiluri și tendințe în design interior preferați să explorați?',
-				acceptedAnswer: {
-					'@type': 'Answer',
-					text: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-				},
-			},
-			{
-				'@type': 'Question',
-				name: 'Cum pot programa o consultație inițială?',
-				acceptedAnswer: {
-					'@type': 'Answer',
-					text: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-				},
-			},
-			{
-				'@type': 'Question',
-				name: 'Cât durează în general un proiect de design interior?',
-				acceptedAnswer: {
-					'@type': 'Answer',
-					text: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-				},
-			},
-		],
+		}));
+
+		return {
+			'@context': 'https://schema.org',
+			'@type': 'FAQPage',
+			mainEntity,
+		};
 	};
 
-	// const FAQCards: IFAQ[] = [
-	// 	{
-	// 		_id:'0',
-	// 		title: 'Cum pot obține o estimare a costurilor pentru proiectul meu?',
-	// 		desc: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-	// 	},
-	// 	{
-	// 		_id:'1',
-	// 		title:
-	// 		'Ce set de abilități și experiență au membrii echipei dumneavoastră?',
-	// 		desc: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-	// 	},
-	// 	{
-	// 		_id:'2',
-	// 		title:
-	// 		'Ce stiluri și tendințe în design interior preferați să explorați?',
-	// 		desc: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-	// 	},
-	// 	{
-	// 		_id:'3',
-	// 		title: 'Cum pot programa o consultație inițială?',
-	// 		desc: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-	// 	},
-	// 	{
-	// 		_id:'4',
-	// 		title: 'Cât durează în general un proiect de design interior?',
-	// 		desc: 'Pentru a obține o estimare a costurilor, vă rugăm să ne contactați și să ne oferiți detalii despre proiectul dumneavoastră. Vom fi bucuroși să vă furnizăm o ofertă personalizată.',
-	// 	},
-	// ];
-
 	const FAQCards: IFAQ[] = await getAllFAQs();
-	console.log(FAQCards)
+
 	return (
 		<section className='sectionM gap-4'>
 			<div className='desktop:m-auto desktop:w-[80%]'>
@@ -115,9 +56,9 @@ export default async function FAQWrapper() {
 			<script
 				type='application/ld+json'
 				dangerouslySetInnerHTML={{
-					__html: JSON.stringify(structuredDataToAdd),
+					__html: JSON.stringify(constructStructureData(FAQCards)),
 				}}
 			/>
 		</section>
 	);
-};
+}

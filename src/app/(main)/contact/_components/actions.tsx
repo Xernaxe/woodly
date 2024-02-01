52f858c0f16def2c55ab450cd1d636f5c73bd24f
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2024-02-01 22:20:16 +0200

    Update actions.tsx

--- src/app/(main)/contact/_components/actions.tsx
+++ src/app/(main)/contact/_components/actions.tsx
@@ -18,71 +18,68 @@ export async function submitFormAction(
 	prevState: FormState,
 	formData: FormData
 ): Promise<FormState> {
-
 	// get form details
 	const name = formData.get('name');
 	const email = formData.get('email');
 	const phoneNumber = formData.get('phoneNumber') as string;
 	const message = formData.get('message');
 
-		// configure transporter
-		const transporter = nodemailer.createTransport({
-			host: 'smtp.gmail.com',
-			secure: true,
-			port: 465,
-			auth: {
-				user: process.env.EMAIL_USER,
-				pass: process.env.EMAIL_PASSWORD,
-			},
-		});
+	// configure transporter
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		secure: true,
+		port: 465,
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASSWORD,
+		},
+	});
 
-		// verify connection configuration
-		await new Promise((resolve, reject) => {
-			transporter.verify(function (error, success) {
-				if (error) {
-					console.log(error);
-					reject(error);
-				} else {
-					console.log("Server is ready to take our messages");
-					resolve(success);
-				}
-			});
+	// verify connection configuration
+	await new Promise((resolve, reject) => {
+		transporter.verify(function (error, success) {
+			if (error) {
+				console.log(error);
+				reject(error);
+			} else {
+				console.log('Server is ready to take our messages');
+				resolve(success);
+			}
 		});
+	});
 
-		const mailOptions = {
-			from: process.env.EMAIL_USER,
-			to: process.env.EMAIL_USER,
-			subject: 'FORMULAR WEBSITE - EMAIL NOU',
-			text: `
+	const mailOptions = {
+		from: process.env.EMAIL_USER,
+		to: process.env.EMAIL_USER,
+		subject: 'FORMULAR WEBSITE - EMAIL NOU',
+		text: `
             Nume: ${name}
             Email: ${email}
             Numar de telefon: ${phoneNumber}
             Mesaj: ${message}
             `,
-		};
+	};
 
-		await new Promise((resolve, reject) => {
-			transporter.sendMail(mailOptions, function (errorz, info) {
-				if (errorz) {
-					console.log(errorz);
-				} else {
-					console.log('Email Sent');
-					return true;
-				}
-			});
-		})
+	await new Promise((resolve, reject) => {
+		transporter.sendMail(mailOptions, function (errorz, info) {
+			if (errorz) {
+				console.log(errorz);
+				reject(errorz);
+			} else {
+				console.log('Email Sent');
+				resolve(true);
+			}
+		});
+	});
 
-
-		return {
-			message: 'success',
-			errors: undefined,
-			fieldValues: {
-				name: '',
-				email: '',
-				phoneNumber: '',
-				message: '',
-			},
-		};
-
-
+	return {
+		message: 'success',
+		errors: undefined,
+		fieldValues: {
+			name: '',
+			email: '',
+			phoneNumber: '',
+			message: '',
+		},
+	};
 }
